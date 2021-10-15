631de62d5aaeb45d83717cfd0aff139e0cebbe0e
elijabesu <41390133+elijabesu@users.noreply.github.com>
2021-10-15 22:59:11 +0200

    PassengerCounterApp

---
+++ PassengerCounterApp/index.js
@@ -0,0 +1,19 @@
+let count = 0
+let countEl = document.getElementById("count-el")
+countEl.innerText = count
+// the DOM = Document Object Model
+let saveEl = document.getElementById("save-el")
+
+console.log(count)
+
+function increment() {
+    count++
+    countEl.innerText = count
+}
+
+function save() {
+    // saveEl.innerText += " " + count + ";"
+    saveEl.textContent += " " + count + ";"
+    count = 0
+    countEl.innerText = "0"
+}
